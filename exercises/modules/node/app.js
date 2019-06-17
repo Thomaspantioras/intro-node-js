1f7187fd146c2ec5b28f799dd3fb8c53122369c0
thomaspantioras <t.pantioras@gmail.com>
2019-06-17 15:55:34 +0100

    excercise2

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,29 @@
+const api = require('./api');
+// const data = require('./data');
+
+const showPostsForCurrentUser = (userId, cb) => {
+    api.getPostsForUser(userId, posts => {
+    const postTemplates = posts.map(post => {
+      return `
+      <div class="post">
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+      </div>`
+    })
+    cb(postTemplates)
+  })
+}
+
+const showUserProfile = (userId, cb) => {
+  api.getUserById(userId, user => {
+    const profile = `
+      <div>
+        ${user.name}
+      </div>
+    `
+    cb(user)
+  })
+}
+
+module.exports = {showPostsForCurrentUser, showUserProfile};
